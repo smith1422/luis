69bc39c12fc56fa8301a17b0e6f3f35d67318891
daniel smith garcia valencia <danielsmith14221422@gmail.com>
2026-01-30 14:10:22 -0500

    Fix conexión DB para Render

--- sistema_administrativo/config.py
+++ sistema_administrativo/config.py
@@ -1,7 +1,9 @@
-db_params = {
-    "host": "localhost",
-    "port": "5433",
-    "dbname": "sistema_administrativo",
-    "user": "postgres",
-    "password": "14221422"
-}
+import os
+import psycopg2
+import urllib.parse
+
+urllib.parse.uses_netloc.append("postgres")
+
+db_url = os.environ.get("DATABASE_URL")
+
+db_params = psycopg2.extensions.parse_dsn(db_url)
